565a4c816338a5144eade08e2e2efa8a54812ba9
Debarchito Nath <dev.debarchito.nath@protonmail.com>
2025-02-14 19:42:37 +0530

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    devenv = {
+      url = "github:cachix/devenv";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs =
+    inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem =
+        { pkgs, ... }:
+        {
+          devenv.shells.default = {
+            packages = with pkgs; [
+              just
+              libxkbcommon
+            ];
+            languages.rust.enable = true;
+          };
+        };
+    };
+}
